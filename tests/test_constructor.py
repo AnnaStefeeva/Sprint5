d9cc35e56a89a45cca153a9e3ed0148f9bc71a88
Anna Stefeeva <laffy@yandex.ru>
2024-06-11 23:10:10 +0500

    Три теста - по одному изолированному на каждый таб

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -3,7 +3,7 @@ import locators
 
 
 class TestTransitionBetweenProduct:
-    def test_transition_between_product(self, driver):
+    def test_transition_between_bun_and_souses(self, driver):
         bun_div = driver.find_element(*locators.BUN_TAB)
         souces_div = driver.find_element(*locators.SOUCES_TAB)
         filling_div = driver.find_element(*locators.FILLINGS_TAB)
@@ -20,6 +20,14 @@ class TestTransitionBetweenProduct:
         assert SELECTED_TAB_CLASS in souces_div.get_attribute('class')
         assert SELECTED_TAB_CLASS not in filling_div.get_attribute('class')
 
+    def test_transition_between_bun_and_fillings(self, driver):
+        bun_div = driver.find_element(*locators.BUN_TAB)
+        souces_div = driver.find_element(*locators.SOUCES_TAB)
+        filling_div = driver.find_element(*locators.FILLINGS_TAB)
+        assert SELECTED_TAB_CLASS in bun_div.get_attribute('class')
+        assert SELECTED_TAB_CLASS not in souces_div.get_attribute('class')
+        assert SELECTED_TAB_CLASS not in filling_div.get_attribute('class')
+
         filling_div.click()
 
         bun_div = driver.find_element(*locators.BUN_TAB)
@@ -29,6 +37,19 @@ class TestTransitionBetweenProduct:
         assert SELECTED_TAB_CLASS not in souces_div.get_attribute('class')
         assert SELECTED_TAB_CLASS in filling_div.get_attribute('class')
 
+    def test_transition_between_fillings_and_bun(self, driver):
+        bun_div = driver.find_element(*locators.BUN_TAB)
+        souces_div = driver.find_element(*locators.SOUCES_TAB)
+        filling_div = driver.find_element(*locators.FILLINGS_TAB)
+        assert SELECTED_TAB_CLASS in bun_div.get_attribute('class')
+        assert SELECTED_TAB_CLASS not in souces_div.get_attribute('class')
+        assert SELECTED_TAB_CLASS not in filling_div.get_attribute('class')
+
+        filling_div.click()
+
+        filling_div = driver.find_element(*locators.FILLINGS_TAB)
+        assert SELECTED_TAB_CLASS in filling_div.get_attribute('class')
+        bun_div = driver.find_element(*locators.BUN_TAB)
         bun_div.click()
 
         bun_div = driver.find_element(*locators.BUN_TAB)
